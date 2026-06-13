dd7206cbcc60f2f40fbf593f09d12016887dac77
navkumarin-cloud <nav.kumar.in@gmail.com>
2026-06-14 02:16:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,23 +8,26 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
-app.get('/', (req, res) => {
-  res.json({
-    status: 'ok'
-  });
-});
-
 app.get('/questions', async (req, res) => {
 
   const { data, error } = await supabase
     .from('questions')
-    .select('*');
+    .select(`
+      question_code,
+      status,
+      current_version,
+      question_versions (
+        version_no,
+        question_text,
+        correct_answer,
+        difficulty_score
+      )
+    `);
 
   res.json({
     data,
     error
   });
-
 });
 
 const PORT = process.env.PORT || 3000;
